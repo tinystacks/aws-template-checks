96e684dcdde98e4fc21bcfd1126f4635808ace79
Caleb Courier <caleb.courier@gmail.com>
2023-01-24 15:04:10 -0600

    handle errors when resolving credentials

--- test/utils/aws/index.test.ts
+++ test/utils/aws/index.test.ts
@@ -28,12 +28,29 @@ describe('aws utils', () => {
       expect(result).toEqual('env-creds');
     });
     it('return credentials from node provider chain if environment credentials are nil', async () => {
-      mockFromEnv.mockReturnValueOnce((): any => undefined);
-      mockFromNodeProviderChain.mockReturnValueOnce(() => 'chain-creds');
+      mockFromEnv.mockReturnValueOnce(async (): Promise<any> => undefined);
+      mockFromNodeProviderChain.mockReturnValueOnce(async () => 'chain-creds');
 
       const result = await getCredentials();
 
       expect(result).toEqual('chain-creds');
+    });
+    it('throws if both node provider chain and environment credentials are nil', async () => {
+      mockFromEnv.mockReturnValueOnce(async () => { throw new Error('Error!') });
+      mockFromNodeProviderChain.mockReturnValueOnce(async (): Promise<any> => undefined);
+
+      let thrownError;
+      try {
+        await getCredentials();
+      } catch (error) {
+        thrownError = error;
+      } finally {
+        expect(thrownError).toBeDefined();
+        expect(thrownError).toHaveProperty('name', 'CliError');
+        expect(thrownError).toHaveProperty('message', 'Failed to detect AWS credentials!');
+        expect(thrownError).toHaveProperty('reason', 'AWS credentials were not found in the environment or anywhere else on the node chain provider.');
+        expect(thrownError).toHaveProperty('hints', ['Make sure you are authenticated to the AWS account you plan to deploy to.']);
+      }
     });
   });
 });
